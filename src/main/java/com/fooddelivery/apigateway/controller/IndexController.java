4a08ba228231ec189b4f3e20db909c8baad4a694
Parthu <parthudhanireddy@gmail.com>
2026-08-06 20:23:28 +0530

    chore: sync all microservices changes

--- src/main/java/com/fooddelivery/apigateway/controller/IndexController.java
+++ src/main/java/com/fooddelivery/apigateway/controller/IndexController.java
@@ -6,8 +6,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class IndexController {
 
     @Value("classpath:/static/index.html")
