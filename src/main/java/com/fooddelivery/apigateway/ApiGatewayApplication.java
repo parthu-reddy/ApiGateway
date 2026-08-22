53b6730b958dd6952cfe9be703aa6f20980973d6
Parthu <parthudhanireddy@gmail.com>
2026-08-22 15:37:09 +0530

    Fix schema validation and configuration errors

--- src/main/java/com/fooddelivery/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/fooddelivery/apigateway/ApiGatewayApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.fooddelivery"})
+@SpringBootApplication(
+    scanBasePackages = {"com.fooddelivery.apigateway", "com.fooddelivery.common"}
+)
 @org.springframework.context.annotation.ComponentScan({"com.fooddelivery", "com.fooddelivery.common"})
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
